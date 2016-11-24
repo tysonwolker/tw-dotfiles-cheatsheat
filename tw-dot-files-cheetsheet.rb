4dbd651043015472604fdd024d8a9291a0168808
Tyson Wolker <tyson.wolker@foxsports.com.au>
2016-11-24 13:10:20 +1100

    Added navigation

--- tw-dot-files-cheetsheet.rb
+++ tw-dot-files-cheetsheet.rb
@@ -15,55 +15,91 @@ cheatsheet do
       command 'git gl'
       name 'a much more useable git log'
     end
+
     entry do
       command 'git b'
       command 'gb'
       name 'a list of branches with summary of last commit'
     end
+
     entry do
       command 'git r'
       name 'a list of remotes with information'
     end
+
     entry do
       command 'git t'
       command 'gt'
       name 'a list of tags iwth information'
     end
+
     entry do
       command 'git nb'
       command 'gnb'
       name 'a (n)ew (b)ranch'
       notes 'like git checkout -b'
     end
+
     entry do
       command 'git cp'
       command 'gcp'
       name 'cherry pick <hash>'
       notes 'Shows what was cherrypicked'
     end
+
     entry do
       command 'git simple'
       name 'a clean format for creating changelogs'
     end
+
     entry do
       command 'git recent-branches'
       name 'if you forgot what you\'ve been working on'
     end
+
     entry do
       command 'git unstage'
       command 'guns'
       name 'remove from index'
     end
+
     entry do
       command 'git uncommit'
       command 'gunc'
       name 'Revert to the time prior to the last commit'
       notes 'This is dangerous if already pushed'
     end
+
     entry do
       command 'gdmb'
       name '(g)it (d)elete (m)erged (b)ranches'
       notes 'Deletes all branches already merged on current branch'
     end
   end
+
+  category do
+    id 'Navigation'  # Must be unique and is used as title of the category
+    entry do
+      command ',z'
+      name 'go to previous buffer'
+    end
+    entry do
+      command ',x'
+      name 'go to next buffer'
+    end
+    entry do
+      command 'Cmd-j'
+      command 'Cmd-k'
+      name 'to move up and down roughly by functions'
+    end
+    entry do
+      command 'Ctrl-o'
+      name 'Old cursor position'
+    end
+    entry do
+      command 'Ctrl-i'
+      name 'New cursor position'
+    end
+  end
+
 end
